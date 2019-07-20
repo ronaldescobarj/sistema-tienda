770c022b5818d6c44919351072d3ffb840b89db2
Ronald Escobar <ronaldescobarj@gmail.com>
2019-07-20 17:16:55 -0400

    minor fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Firebase, { FirebaseContext } from './components/Firebase';
+import Firebase, { FirebaseContext } from './components/firebase';
 
 ReactDOM.render(
     <FirebaseContext.Provider value={new Firebase()}>
