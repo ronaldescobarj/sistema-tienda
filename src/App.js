3152c20485c9eb4354eea490a1673b9674bd9236
Ronald Escobar <ronaldescobarj@gmail.com>
2019-07-28 18:56:38 -0400

    added state of authUser to app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.sass';
 import Header from './components/header/header';
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -9,22 +9,46 @@ import AddItem from './components/add-item/add-item';
 import EditItem from './components/edit-item/edit-item';
 import EditAccount from './components/edit-account/edit-account';
 import LoginPage from './components/login/login';
+import { withFirebase } from './components/firebase';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Route exact path="/" component={MainDashboard} />
-        <Route path="/iniciar-sesion" component={LoginPage} />
-        <Route path="/inventario" component={Inventory} />
-        <Route path="/nuevo-item" component={AddItem} />
-        <Route path="/registro-ventas" component={SalesRecord} />
-        <Route path="/item/:id" component={EditItem} />
-        <Route path="/editar-cuenta" component={EditAccount} />
-      </div>
-    </Router>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      authUser: null
+    }
+  }
+
+  componentDidMount() {
+    this.listener = this.props.firebase.auth.onAuthStateChanged(authUser => {
+      authUser
+        ? this.setState({ authUser })
+        : this.setState({ authUser: null });
+    });
+  }
+
+  componentWillUnmount() {
+    this.listener();
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header authUser={this.state.authUser} />
+          <Route exact path="/" component={MainDashboard} />
+          <Route path="/iniciar-sesion" component={LoginPage} />
+          <Route path="/inventario" component={Inventory} />
+          <Route path="/nuevo-item" component={AddItem} />
+          <Route path="/registro-ventas" component={SalesRecord} />
+          <Route path="/item/:id" component={EditItem} />
+          <Route path="/editar-cuenta" component={EditAccount} />
+        </div>
+      </Router>
+    );
+  }
+
 }
 
-export default App;
+export default withFirebase(App);
